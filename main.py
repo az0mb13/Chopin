c3ff2ce6390a12a90de43d671b22a198e946f91e
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2021-10-18 01:12:17 +0530

    Extraction of hostnames done

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ args = parser.parse_args()
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 #Converting CIDR to IPs and storing in ips.txt
-f = open("ips.txt", "w")
-subprocess.run(["cidr2ip", "-f", args.file], stdout=f)
-
+cidr2ip = open("ips.txt", "w")
+subprocess.run(["cidr2ip", "-f", args.file], stdout=cidr2ip)
+cidr2ip.close()
 
 #Adding CIDRs and IPs to Shodan Monitor
 shodan_api = Shodan(shodan_key) 
 with open(args.file) as sh:
     lines = sh.read().splitlines()
     #Creating alert
-    # create_alert = shodan_api.create_alert(name=args.project, ip=lines, expires=0)
-    # alert_id = create_alert["id"]
-    # #Enabling triggers
-    # shodan_api.enable_alert_trigger(aid=alert_id, trigger='industrial_control_system,internet_scanner,iot,malware,new_service,open_database,ssl_expired,uncommon,uncommon_plus,vulnerable,vulnerable_unverified')
-    # #Enabling Slack Webhook for triggers
-    # shodan_api.add_alert_notifier(aid=alert_id, nid='yUCrcHttQhRVuexc')
+    create_alert = shodan_api.create_alert(name=args.project, ip=lines, expires=0)
+    alert_id = create_alert["id"]
+    #Enabling triggers
+    shodan_api.enable_alert_trigger(aid=alert_id, trigger='industrial_control_system,internet_scanner,iot,malware,new_service,open_database,ssl_expired,uncommon,uncommon_plus,vulnerable,vulnerable_unverified')
+    #Enabling Slack Webhook for triggers
+    shodan_api.add_alert_notifier(aid=alert_id, nid='yUCrcHttQhRVuexc')
 
     query_string = "net:"+(",".join(lines))
     query_out = shodan_api.search(query=query_string, page=1)
@@ -44,17 +44,50 @@ with open(args.file) as sh:
         hostfile.write(element + "\n")
     hostfile.close()
 
-#Fetching certs from SSL
+# Fetching certs from SSL
 cero = open("ssl_hosts_temp.txt", "w")
 cero_in = open(args.file, "r")
 subprocess.run(["cero"], stdout=cero, stdin=cero_in)
 
 with open("ssl_hosts_temp.txt") as ss:
     f = open("ssl_hosts.txt", "w")
-    lines= ss.read().splitlines()
-    for e in list(set(lines)):
-        f.write(e + "\n")
+    sslines= ss.read().splitlines()
+    for sse in list(set(sslines)):
+        f.write(sse + "\n")
     f.close()
 os.remove("ssl_hosts_temp.txt")
 
+#Reverse DNS lookup
+hakrev_in = open("ips.txt", "r")
+hakrev_out = open("revdns_temp.txt", "w")
+subprocess.run(["hakrevdns", "-d"], stdout=hakrev_out, stdin=hakrev_in)
 
+with open("revdns_temp.txt") as hr:
+    f = open("revdns.txt", "w")
+    hrlines= hr.read().splitlines()
+    for hre in list(set(hrlines)):
+        if "in-addr.arpa" not in hre:
+            f.write(hre + "\n")
+    f.close()
+os.remove("revdns_temp.txt")
+
+#Unique all hosts
+h1 = open("ssl_hosts.txt", "r")
+h1Content = h1.read()
+h2 = open("revdns.txt", "r")
+h2Content = h2.read()
+h3 = open("hostnames.txt", "r")
+h3Content = h3.read()
+h1List = h1Content.splitlines()
+h2List = h2Content.splitlines()
+h3List = h3Content.splitlines()
+h1.close()
+h2.close()
+h3.close()
+hosts_final = list(set(h1List+h2List+h3List))
+h_final = open("hosts_final.txt", "w")
+for item in hosts_final: 
+    h_final.write(item + "\n")
+h_final.close()
+
+#Port scan using masscan
