b8996598cb7e167195f628c812963ab11a0a5777
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2021-11-16 19:06:42 +0530

    Setup created

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install shodan
+go get -u github.com/codeexpress/cidr2ip
+go get -u github.com/glebarez/cero
+go get github.com/hakluke/hakrevdns
+Install masscan manually
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
